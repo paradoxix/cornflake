45c2ba3f20113e50ac930b2111921156feb6f4bc
Lars Reichardt <paranoiaix@gmail.com>
2016-07-02 17:57:29 +0200

    pass config to newly created CornFlake-instances, validate config

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,116 @@
-#[macro_use]
-extern crate log;
 extern crate time;
 
 use std::default::Default;
+use std::error::Error as StdError;
+use std::fmt;
+use std::result;
 
-// EPOCH is 2016-01-01T00:00:00.000
-static EPOCH: u64 = 1451602800000;
+// DEFAULT_EPOCH is 2016-01-01T00:00:00.000
+static DEFAULT_EPOCH: u64 = 1451602800;
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub node_id_bits: i8,
+    pub sequence_bits: i8,
+    pub node_id: u64,
+    pub epoch: u64,
+}
+
+impl fmt::Display for Config {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "config-{:x}-{:x}-{:x}", self.node_id_bits, self.node_id, self.sequence_bits)
+    }
+}
 
 #[derive(Debug)]
 pub struct CornFlake {
-    node_id_bits: i8,
-    sequence_bits: i8,
-
     node_id: u64,
     sequence: u64,
-
-    timestamp_left_shift: i8,
     sequence_mask: u64,
-
     last_timestamp: u64,
+    epoch: u64,
+
+    node_id_left_shift: i8,
+    timestamp_left_shift: i8,
 }
 
-impl Default for CornFlake {
-    fn default() -> CornFlake {
-        CornFlake {
+impl fmt::Display for CornFlake {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "flake-{:x}-{:x}-{:x}", self.last_timestamp, self.node_id, self.sequence)
+    }
+}
+
+#[derive(Debug)]
+pub enum CornFlakeError {
+    TooFewTimestampBits,
+    NodeIdTooBig(u64),
+    ClockRunningBackwards,
+}
+
+impl fmt::Display for CornFlakeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &CornFlakeError::TooFewTimestampBits => write!(f, "TooFewTimestampBits (less then 41bit)"),
+            &CornFlakeError::NodeIdTooBig(ref id) => write!(f, "NodeIdTooBig: {}", id),
+            &CornFlakeError::ClockRunningBackwards => write!(f, "Clock running backwards!!!"),
+        }
+    }
+}
+
+impl StdError for CornFlakeError {
+    fn description(&self) -> &str {
+        match self {
+            &CornFlakeError::TooFewTimestampBits => "TooFewTimestampBits (less then 41bit)",
+            &CornFlakeError::NodeIdTooBig(_) => "NodeIdTooBig",
+            &CornFlakeError::ClockRunningBackwards => "Clock running backwards!!!",
+        }
+    }
+}
+
+pub type Result<T> = result::Result<T, CornFlakeError>;
+
+impl Default for Config {
+    fn default() -> Config {
+        Config {
             node_id_bits: 10,
             sequence_bits: 10,
-
             node_id: 0,
-            sequence: 0,
-
-            timestamp_left_shift: 20,
-            sequence_mask: !0 ^ (!0 << 10),
-
-            last_timestamp: 0,
+            epoch: DEFAULT_EPOCH,
         }
     }
 }
 
 impl CornFlake {
-    pub fn new(node_id: u64) -> CornFlake {
-        let mut flake: CornFlake = Default::default();
+    pub fn new(config: &Config) -> Result<CornFlake> {
+        if config.node_id_bits + config.sequence_bits > 22 {
+            return Err(CornFlakeError::TooFewTimestampBits);
+        }
+        if config.node_id > (1 << config.node_id_bits) {
+            return Err(CornFlakeError::NodeIdTooBig(config.node_id));
+        }
 
-        assert!(node_id < (1 << flake.node_id_bits));
+        Ok(CornFlake {
+            node_id: config.node_id,
+            sequence: 0,
+            last_timestamp: 0,
+            epoch: config.epoch,
 
-        flake.node_id = node_id;
+            node_id_left_shift: config.node_id_bits,
+            timestamp_left_shift: config.node_id_bits + config.sequence_bits,
+            sequence_mask: !0 ^ (!0 << config.sequence_bits),
+        })
+    }
 
-        flake
+    #[inline(always)]
+    fn epoch_timestamp(&self) -> u64 {
+        let t = time::get_time();
+        ((t.sec as u64 - self.epoch) * 1000) + (t.nsec / 1000000) as u64
     }
 
     fn til_next_ms(&self) -> u64 {
-        let mut timestamp = time::precise_time_ns() * 1000000;
+        let mut timestamp = self.epoch_timestamp();
         while timestamp <= self.last_timestamp {
-            timestamp = time::precise_time_ns() * 1000000;
+            timestamp = self.epoch_timestamp();
         }
         timestamp
     }
@@ -61,12 +119,11 @@ impl CornFlake {
         self.node_id
     }
 
-    pub fn next_id(&mut self) -> u64 {
-        let mut timestamp = time::precise_time_ns();
+    pub fn next_id(&mut self) -> Result<u64> {
+        let mut timestamp = self.epoch_timestamp();
 
         if timestamp < self.last_timestamp {
-            error!("clock running backwards!!!");
-            timestamp = self.til_next_ms();
+            return Err(CornFlakeError::ClockRunningBackwards);
         }
 
         if timestamp == self.last_timestamp {
@@ -80,23 +137,23 @@ impl CornFlake {
 
         self.last_timestamp = timestamp;
 
-        return ((timestamp - EPOCH) << self.timestamp_left_shift) |
-            (self.node_id << self.node_id_bits) | self.sequence;
+        Ok((timestamp << self.timestamp_left_shift) | (self.node_id << self.node_id_left_shift) | self.sequence)
     }
 }
 
 
-
 #[cfg(test)]
 mod tests {
+    use super::Config;
     use super::CornFlake;
 
     #[test]
-    fn it_works() {
-        let mut f = CornFlake::new(1);
+    fn initialize_and_run_default() {
+        let c: Config = Default::default();
+        let mut f = CornFlake::new(&c).unwrap();
 
-        for _ in 1..100000 {
-            println!("{} ", f.next_id());
+        for _ in 1..100 {
+            println!("{} ", f.next_id().unwrap());
         }
     }
 }
